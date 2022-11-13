e9b17a278824fb317132cc301e9f1d585aea3bdd
kkoon9 <rndrnjs2003@gmail.com>
2022-11-13 13:30:33 +0900

    feat: AdvancedSystemCommandJob
    Signed-off-by: kkoon9 <rndrnjs2003@gmail.com>

---
+++ src/main/java/com/kkoon9/springbatch/batch/SystemCommandJob.java
@@ -0,0 +1,51 @@
+package com.kkoon9.springbatch.batch;
+
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.step.tasklet.SystemCommandTasklet;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@EnableBatchProcessing
+@SpringBootApplication
+public class SystemCommandJob {
+    @Autowired
+    public JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    public StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job job() {
+        return this.jobBuilderFactory.get("systemCommandJob")
+                .start(systemCommandStep())
+                .build();
+    }
+
+    private Step systemCommandStep() {
+        return this.stepBuilderFactory.get("systemCommandStep")
+                .tasklet(systemCommandTasklet())
+                .build();
+    }
+
+    @Bean
+    public SystemCommandTasklet systemCommandTasklet() {
+        SystemCommandTasklet systemCommandTasklet = new SystemCommandTasklet();
+
+        systemCommandTasklet.setCommand("rm -rf /tmp.txt");
+        systemCommandTasklet.setTimeout(5000);
+        systemCommandTasklet.setInterruptOnCancel(true);
+
+        return systemCommandTasklet;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SystemCommandJob.class, args);
+    }
+}
