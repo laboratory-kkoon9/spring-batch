ea9b597d4d857487470fb73c750302c8f8b5ab76
kkoon9 <rndrnjs2003@gmail.com>
2022-11-13 13:49:38 +0900

    feat: 청크 기반 기본 예제
    Signed-off-by: kkoon9 <rndrnjs2003@gmail.com>

--- src/main/java/com/kkoon9/springbatch/batch/BatchConfiguration.java
+++ src/main/java/com/kkoon9/springbatch/batch/BatchConfiguration.java
@@ -5,11 +5,20 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.listener.ExecutionContextPromotionListener;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
+import org.springframework.batch.item.file.mapping.PassThroughLineMapper;
+import org.springframework.batch.item.file.transform.PassThroughLineAggregator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 
 @EnableBatchProcessing
 @Configuration
@@ -27,13 +36,33 @@ public class BatchConfiguration {
                 .build();
     }
 
-    private Step step1() {
+    @Bean
+    public Step step1() {
         return this.stepBuilderFactory.get("step1")
-                .tasklet(helloWorldTasket())
+                .<String, String>chunk(10)
+                .reader(itemReader(null))
+                .writer(itemWriter(null))
                 .build();
     }
 
-    private Tasklet helloWorldTasket() {
-        return new HelloWorld();
+    @Bean
+    @StepScope
+    public FlatFileItemReader<String> itemReader(@Value("#{jobParameters['inputFile']}") Resource inputFile) {
+        return new FlatFileItemReaderBuilder<String>()
+                .name("itemReader")
+                .resource(inputFile)
+                .lineMapper(new PassThroughLineMapper())
+                .build();
+
+    }
+
+    @Bean
+    @StepScope
+    public FlatFileItemWriter<String> itemWriter(@Value("#{jobParameters['outputFile']}") Resource outputFile) {
+        return new FlatFileItemWriterBuilder<String>()
+                .name("itemWriter")
+                .resource(outputFile)
+                .lineAggregator(new PassThroughLineAggregator<>())
+                .build();
     }
 }
